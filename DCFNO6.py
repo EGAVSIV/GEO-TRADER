38ae6e1360547454680bc0355d785f17d1ed7e4a
Gaurav Singh Yadav <137716439+EGAVSIV@users.noreply.github.com>
2026-01-19 13:51:48 +0530

    Update DCFNO6.py

--- DCFNO6.py
+++ DCFNO6.py
@@ -39,8 +39,8 @@ for tf in TIMEFRAMES:
 
 fno_symbols = [ 
 
-    'LICHSGFIN','LICI','LODHA','LT','LTF','LUPIN','M&M','MANAPPURAM','MANKIND','MARICO','MARUTI','MAXHEALTH',
-    'MAZDOCK','MCX','MFSL','MOTHERSON','MPHASIS','MUTHOOTFIN','NATIONALUM','NAUKRI','NBCC','NCC','NESTLEIND']
+    'NMDC','NTPC','NUVAMA','NYKAA','OBEROIRLTY','OFSS','OIL','ONGC','PAGEIND','PATANJALI','PAYTM',
+    'PFC','PGEL','PHOENIXLTD','PIIND','PNB','PNBHOUSING','POLICYBZR','POLYCAB','NHPC','HCLTECH','POWERGRID']
 # =====================================================
 # INDICATORS
 # =====================================================
@@ -111,3 +111,4 @@ if __name__ == "__main__":
 
 
     
+
